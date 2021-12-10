16653fea4941107588c92f499271911d9c3c6e54
Bcodemaker <bcodemaker@gmail.com>
2021-12-10 16:23:34 +0530

    data base my sqli

---
+++ procokr7_university.sql
@@ -0,0 +1,699 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 10, 2021 at 09:30 AM
+-- Server version: 5.7.23-23
+-- PHP Version: 7.3.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `procokr7_university`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `gender` varchar(150) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `avatar` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `role_id`, `name`, `email`, `date_of_birth`, `gender`, `mobile`, `password`, `status`, `avatar`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, 'Super Admin', 'asif.sanix@gmail.com', NULL, NULL, '7894561230', '$2y$10$XZuiRROY/vGwCjI4ImzgFO9d4To3vDfmdspRC7ABueeqykhhzNt46', 1, NULL, '2019-03-27 00:00:00', '2019-03-28 09:04:06', NULL),
+(2, 2, 'Asif Jamal', 'asifjamal@yopmail.com', NULL, NULL, NULL, '$2y$10$XOdmM/9XvRgKzgE3uVTf9O4NMk48slxr7/8SMKR/LG0EY2nImMLSK', 1, NULL, '2019-08-08 17:09:35', '2019-08-08 17:27:11', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_details`
+--
+
+CREATE TABLE `admin_details` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `address` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_password_resets`
+--
+
+CREATE TABLE `admin_password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admin_password_resets`
+--
+
+INSERT INTO `admin_password_resets` (`email`, `token`, `created_at`) VALUES
+('asifjamal@yopmail.com', '$2y$10$J/boDihlUPMgBF7L2uPVjeaYlLxKjQ4Ifo25TfhwBOt3UCix0kkYm', '2019-06-16 12:06:57'),
+('asif.sanix@gmail.com', '$2y$10$cNoidIFR/27b5zYgCugNGeto1P2Dr0uHNen4gUahwteuP1vCgURlm', '2019-08-08 06:19:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_role`
+--
+
+CREATE TABLE `admin_role` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admin_role`
+--
+
+INSERT INTO `admin_role` (`id`, `admin_id`, `role_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, NULL, NULL),
+(2, 2, 2, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `sortname` varchar(3) NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `sortname`, `name`) VALUES
+(1, 'AF', 'Afghanistan'),
+(2, 'AL', 'Albania'),
+(3, 'DZ', 'Algeria'),
+(4, 'AS', 'American Samoa'),
+(5, 'AD', 'Andorra'),
+(6, 'AO', 'Angola'),
+(7, 'AI', 'Anguilla'),
+(8, 'AQ', 'Antarctica'),
+(9, 'AG', 'Antigua And Barbuda'),
+(10, 'AR', 'Argentina'),
+(11, 'AM', 'Armenia'),
+(12, 'AW', 'Aruba'),
+(13, 'AU', 'Australia'),
+(14, 'AT', 'Austria'),
+(15, 'AZ', 'Azerbaijan'),
+(16, 'BS', 'Bahamas The'),
+(17, 'BH', 'Bahrain'),
+(18, 'BD', 'Bangladesh'),
+(19, 'BB', 'Barbados'),
+(20, 'BY', 'Belarus'),
+(21, 'BE', 'Belgium'),
+(22, 'BZ', 'Belize'),
+(23, 'BJ', 'Benin'),
+(24, 'BM', 'Bermuda'),
+(25, 'BT', 'Bhutan'),
+(26, 'BO', 'Bolivia'),
+(27, 'BA', 'Bosnia and Herzegovina'),
+(28, 'BW', 'Botswana'),
+(29, 'BV', 'Bouvet Island'),
+(30, 'BR', 'Brazil'),
+(31, 'IO', 'British Indian Ocean Territory'),
+(32, 'BN', 'Brunei'),
+(33, 'BG', 'Bulgaria'),
+(34, 'BF', 'Burkina Faso'),
+(35, 'BI', 'Burundi'),
+(36, 'KH', 'Cambodia'),
+(37, 'CM', 'Cameroon'),
+(38, 'CA', 'Canada'),
+(39, 'CV', 'Cape Verde'),
+(40, 'KY', 'Cayman Islands'),
+(41, 'CF', 'Central African Republic'),
+(42, 'TD', 'Chad'),
+(43, 'CL', 'Chile'),
+(44, 'CN', 'China'),
+(45, 'CX', 'Christmas Island'),
+(46, 'CC', 'Cocos (Keeling) Islands'),
+(47, 'CO', 'Colombia'),
+(48, 'KM', 'Comoros'),
+(49, 'CG', 'Congo'),
+(50, 'CD', 'Congo The Democratic Republic Of The'),
+(51, 'CK', 'Cook Islands'),
+(52, 'CR', 'Costa Rica'),
+(53, 'CI', 'Cote D\'Ivoire (Ivory Coast)'),
+(54, 'HR', 'Croatia (Hrvatska)'),
+(55, 'CU', 'Cuba'),
+(56, 'CY', 'Cyprus'),
+(57, 'CZ', 'Czech Republic'),
+(58, 'DK', 'Denmark'),
+(59, 'DJ', 'Djibouti'),
+(60, 'DM', 'Dominica'),
+(61, 'DO', 'Dominican Republic'),
+(62, 'TP', 'East Timor'),
+(63, 'EC', 'Ecuador'),
+(64, 'EG', 'Egypt'),
+(65, 'SV', 'El Salvador'),
+(66, 'GQ', 'Equatorial Guinea'),
+(67, 'ER', 'Eritrea'),
+(68, 'EE', 'Estonia'),
+(69, 'ET', 'Ethiopia'),
+(70, 'XA', 'External Territories of Australia'),
+(71, 'FK', 'Falkland Islands'),
+(72, 'FO', 'Faroe Islands'),
+(73, 'FJ', 'Fiji Islands'),
+(74, 'FI', 'Finland'),
+(75, 'FR', 'France'),
+(76, 'GF', 'French Guiana'),
+(77, 'PF', 'French Polynesia'),
+(78, 'TF', 'French Southern Territories'),
+(79, 'GA', 'Gabon'),
+(80, 'GM', 'Gambia The'),
+(81, 'GE', 'Georgia'),
+(82, 'DE', 'Germany'),
+(83, 'GH', 'Ghana'),
+(84, 'GI', 'Gibraltar'),
+(85, 'GR', 'Greece'),
+(86, 'GL', 'Greenland'),
+(87, 'GD', 'Grenada'),
+(88, 'GP', 'Guadeloupe'),
+(89, 'GU', 'Guam'),
+(90, 'GT', 'Guatemala'),
+(91, 'XU', 'Guernsey and Alderney'),
+(92, 'GN', 'Guinea'),
+(93, 'GW', 'Guinea-Bissau'),
+(94, 'GY', 'Guyana'),
+(95, 'HT', 'Haiti'),
+(96, 'HM', 'Heard and McDonald Islands'),
+(97, 'HN', 'Honduras'),
+(98, 'HK', 'Hong Kong S.A.R.'),
+(99, 'HU', 'Hungary'),
+(100, 'IS', 'Iceland'),
+(101, 'IN', 'India'),
+(102, 'ID', 'Indonesia'),
+(103, 'IR', 'Iran'),
+(104, 'IQ', 'Iraq'),
+(105, 'IE', 'Ireland'),
+(106, 'IL', 'Israel'),
+(107, 'IT', 'Italy'),
+(108, 'JM', 'Jamaica'),
+(109, 'JP', 'Japan'),
+(110, 'XJ', 'Jersey'),
+(111, 'JO', 'Jordan'),
+(112, 'KZ', 'Kazakhstan'),
+(113, 'KE', 'Kenya'),
+(114, 'KI', 'Kiribati'),
+(115, 'KP', 'Korea North'),
+(116, 'KR', 'Korea South'),
+(117, 'KW', 'Kuwait'),
+(118, 'KG', 'Kyrgyzstan'),
+(119, 'LA', 'Laos'),
+(120, 'LV', 'Latvia'),
+(121, 'LB', 'Lebanon'),
+(122, 'LS', 'Lesotho'),
+(123, 'LR', 'Liberia'),
+(124, 'LY', 'Libya'),
+(125, 'LI', 'Liechtenstein'),
+(126, 'LT', 'Lithuania'),
+(127, 'LU', 'Luxembourg'),
+(128, 'MO', 'Macau S.A.R.'),
+(129, 'MK', 'Macedonia'),
+(130, 'MG', 'Madagascar'),
+(131, 'MW', 'Malawi'),
+(132, 'MY', 'Malaysia'),
+(133, 'MV', 'Maldives'),
+(134, 'ML', 'Mali'),
+(135, 'MT', 'Malta'),
+(136, 'XM', 'Man (Isle of)'),
+(137, 'MH', 'Marshall Islands'),
+(138, 'MQ', 'Martinique'),
+(139, 'MR', 'Mauritania'),
+(140, 'MU', 'Mauritius'),
+(141, 'YT', 'Mayotte'),
+(142, 'MX', 'Mexico'),
+(143, 'FM', 'Micronesia'),
+(144, 'MD', 'Moldova'),
+(145, 'MC', 'Monaco'),
+(146, 'MN', 'Mongolia'),
+(147, 'MS', 'Montserrat'),
+(148, 'MA', 'Morocco'),
+(149, 'MZ', 'Mozambique'),
+(150, 'MM', 'Myanmar'),
+(151, 'NA', 'Namibia'),
+(152, 'NR', 'Nauru'),
+(153, 'NP', 'Nepal'),
+(154, 'AN', 'Netherlands Antilles'),
+(155, 'NL', 'Netherlands The'),
+(156, 'NC', 'New Caledonia'),
+(157, 'NZ', 'New Zealand'),
+(158, 'NI', 'Nicaragua'),
+(159, 'NE', 'Niger'),
+(160, 'NG', 'Nigeria'),
+(161, 'NU', 'Niue'),
+(162, 'NF', 'Norfolk Island'),
+(163, 'MP', 'Northern Mariana Islands'),
+(164, 'NO', 'Norway'),
+(165, 'OM', 'Oman'),
+(166, 'PK', 'Pakistan'),
+(167, 'PW', 'Palau'),
+(168, 'PS', 'Palestinian Territory Occupied'),
+(169, 'PA', 'Panama'),
+(170, 'PG', 'Papua new Guinea'),
+(171, 'PY', 'Paraguay'),
+(172, 'PE', 'Peru'),
+(173, 'PH', 'Philippines'),
+(174, 'PN', 'Pitcairn Island'),
+(175, 'PL', 'Poland'),
+(176, 'PT', 'Portugal'),
+(177, 'PR', 'Puerto Rico'),
+(178, 'QA', 'Qatar'),
+(179, 'RE', 'Reunion'),
+(180, 'RO', 'Romania'),
+(181, 'RU', 'Russia'),
+(182, 'RW', 'Rwanda'),
+(183, 'SH', 'Saint Helena'),
+(184, 'KN', 'Saint Kitts And Nevis'),
+(185, 'LC', 'Saint Lucia'),
+(186, 'PM', 'Saint Pierre and Miquelon'),
+(187, 'VC', 'Saint Vincent And The Grenadines'),
+(188, 'WS', 'Samoa'),
+(189, 'SM', 'San Marino'),
+(190, 'ST', 'Sao Tome and Principe'),
+(191, 'SA', 'Saudi Arabia'),
+(192, 'SN', 'Senegal'),
+(193, 'RS', 'Serbia'),
+(194, 'SC', 'Seychelles'),
+(195, 'SL', 'Sierra Leone'),
+(196, 'SG', 'Singapore'),
+(197, 'SK', 'Slovakia'),
+(198, 'SI', 'Slovenia'),
+(199, 'XG', 'Smaller Territories of the UK'),
+(200, 'SB', 'Solomon Islands'),
+(201, 'SO', 'Somalia'),
+(202, 'ZA', 'South Africa'),
+(203, 'GS', 'South Georgia'),
+(204, 'SS', 'South Sudan'),
+(205, 'ES', 'Spain'),
+(206, 'LK', 'Sri Lanka'),
+(207, 'SD', 'Sudan'),
+(208, 'SR', 'Suriname'),
+(209, 'SJ', 'Svalbard And Jan Mayen Islands'),
+(210, 'SZ', 'Swaziland'),
+(211, 'SE', 'Sweden'),
+(212, 'CH', 'Switzerland'),
+(213, 'SY', 'Syria'),
+(214, 'TW', 'Taiwan'),
+(215, 'TJ', 'Tajikistan'),
+(216, 'TZ', 'Tanzania'),
+(217, 'TH', 'Thailand'),
+(218, 'TG', 'Togo'),
+(219, 'TK', 'Tokelau'),
+(220, 'TO', 'Tonga'),
+(221, 'TT', 'Trinidad And Tobago'),
+(222, 'TN', 'Tunisia'),
+(223, 'TR', 'Turkey'),
+(224, 'TM', 'Turkmenistan'),
+(225, 'TC', 'Turks And Caicos Islands'),
+(226, 'TV', 'Tuvalu'),
+(227, 'UG', 'Uganda'),
+(228, 'UA', 'Ukraine'),
+(229, 'AE', 'United Arab Emirates'),
+(230, 'GB', 'United Kingdom'),
+(231, 'US', 'United States'),
+(232, 'UM', 'United States Minor Outlying Islands'),
+(233, 'UY', 'Uruguay'),
+(234, 'UZ', 'Uzbekistan'),
+(235, 'VU', 'Vanuatu'),
+(236, 'VA', 'Vatican City State (Holy See)'),
+(237, 'VE', 'Venezuela'),
+(238, 'VN', 'Vietnam'),
+(239, 'VG', 'Virgin Islands (British)'),
+(240, 'VI', 'Virgin Islands (US)'),
+(241, 'WF', 'Wallis And Futuna Islands'),
+(242, 'EH', 'Western Sahara'),
+(243, 'YE', 'Yemen'),
+(244, 'YU', 'Yugoslavia'),
+(245, 'ZM', 'Zambia'),
+(246, 'ZW', 'Zimbabwe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menus`
+--
+
+CREATE TABLE `menus` (
+  `slug` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `icon` varchar(255) DEFAULT NULL,
+  `parent` varchar(255) DEFAULT NULL,
+  `ordering` int(11) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menus`
+--
+
+INSERT INTO `menus` (`slug`, `name`, `icon`, `parent`, `ordering`, `status`) VALUES
+('admin', 'Admin', 'mdi mdi-account-tie', NULL, 3, 1),
+('bread', 'Bread', 'ft-target', 'setting', 2, 1),
+('dashboard', 'Dashboard', 'bx bxs-dashboard', NULL, 0, 1),
+('menu', 'Menu', NULL, 'setting', 1, 1),
+('role', 'Role', NULL, 'setting', 0, 1),
+('setting', 'Setting', 'bx bx-cog', NULL, 7, 1),
+('site_setting', 'Site Setting', 'bx bx-wrench', NULL, 6, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(11) NOT NULL,
+  `menu_slug` varchar(200) DEFAULT NULL,
+  `permission_key` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `menu_slug`, `permission_key`) VALUES
+(1, 'role', 'browse_role'),
+(2, 'role', 'read_role'),
+(3, 'role', 'add_role'),
+(4, 'role', 'edit_role'),
+(5, 'role', 'delete_role'),
+(6, 'menu', 'browse_menu'),
+(7, 'menu', 'read_menu'),
+(8, 'menu', 'add_menu'),
+(9, 'menu', 'edit_menu'),
+(10, 'menu', 'delete_menu'),
+(11, 'setting', 'browse_setting'),
+(12, 'dashboard', 'browse_dashboard'),
+(13, 'bread', 'browse_bread'),
+(14, 'bread', 'read_bread'),
+(15, 'bread', 'add_bread'),
+(16, 'bread', 'edit_bread'),
+(17, 'bread', 'delete_bread'),
+(18, 'site_setting', 'browse_site_setting'),
+(19, 'site_setting', 'read_site_setting'),
+(20, 'site_setting', 'add_site_setting'),
+(21, 'site_setting', 'edit_site_setting'),
+(22, 'site_setting', 'delete_site_setting'),
+(23, 'site_setting', 'logo_site_setting'),
+(24, 'admin', 'browse_admin'),
+(25, 'admin', 'read_admin'),
+(26, 'admin', 'add_admin'),
+(27, 'admin', 'edit_admin'),
+(28, 'admin', 'delete_admin'),
+(29, 'admin', 'profile_admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `display_name` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `display_name`, `created_at`, `updated_at`) VALUES
+(1, 'root', 'Super Admin', '2019-03-28 06:21:03', '2019-03-28 06:21:03'),
+(2, 'Admin', 'Admin', '2019-08-08 16:03:49', '2019-08-08 16:03:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_permissions`
+--
+
+CREATE TABLE `role_permissions` (
+  `role_id` int(11) NOT NULL,
+  `permission_key` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `role_permissions`
+--
+
+INSERT INTO `role_permissions` (`role_id`, `permission_key`) VALUES
+(1, 'add_admin'),
+(1, 'add_bread'),
+(1, 'add_client'),
+(1, 'add_menu'),
+(1, 'add_role'),
+(1, 'add_site_setting'),
+(1, 'browse_admin'),
+(1, 'browse_bread'),
+(1, 'browse_dashboard'),
+(1, 'browse_menu'),
+(1, 'browse_role'),
+(1, 'browse_setting'),
+(1, 'browse_site_setting'),
+(1, 'delete_admin'),
+(1, 'delete_bread'),
+(1, 'delete_menu'),
+(1, 'delete_project'),
+(1, 'delete_role'),
+(1, 'delete_site_setting'),
+(1, 'edit_admin'),
+(1, 'edit_bread'),
+(1, 'edit_menu'),
+(1, 'edit_role'),
+(1, 'edit_site_setting'),
+(1, 'logo_site_setting'),
+(1, 'profile_admin'),
+(1, 'read_admin'),
+(1, 'read_bread'),
+(1, 'read_menu'),
+(1, 'read_role'),
+(1, 'read_site_setting'),
+(2, 'browse_dashboard');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site_settings`
+--
+
+CREATE TABLE `site_settings` (
+  `id` int(11) NOT NULL,
+  `site_title` varchar(255) DEFAULT NULL,
+  `site_description` text,
+  `logo` varchar(255) DEFAULT NULL,
+  `favicon` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `site_settings`
+--
+
+INSERT INTO `site_settings` (`id`, `site_title`, `site_description`, `logo`, `favicon`, `created_at`, `updated_at`) VALUES
+(1, 'The Code', 'We Deliver Ideas', 'storage/sitesetting/1625565635_logo.png', 'storage/sitesetting/1603732301_favicon.png', NULL, '2021-07-06 10:00:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `device_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `device_token`, `created_at`, `updated_at`) VALUES
+(1, 'Asif', 'asif.sanix@gmail.com', '$2y$10$c1Ju9HZhN00TUK9DCtmSLeOwGwjPcXA5yNr4ZMRqF04f9PQIX8Q6C', NULL, 'c7HFjroeU8CokWAwCzagD2:APA91bG--o9PKRKpTD6Nd7c4IgGCSr_x0fw6fq_fQK2ySWJ2OH0U-RRIiT7wPHwpKc0rfnDZBo6OruCDxbQj6E0kyM4dINgvPMGS2ObdGGX4MEcJMBrR4JrFpvb3UtJ0bt8JTyOqPWR3', '2020-10-26 03:14:52', '2020-10-26 03:14:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin_details`
+--
+ALTER TABLE `admin_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin_password_resets`
+--
+ALTER TABLE `admin_password_resets`
+  ADD KEY `password_resets_email_index` (`email`(191));
+
+--
+-- Indexes for table `admin_role`
+--
+ALTER TABLE `admin_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`slug`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `key` (`permission_key`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `role_permissions`
+--
+ALTER TABLE `role_permissions`
+  ADD UNIQUE KEY `role_id_2` (`role_id`,`permission_key`);
+
+--
+-- Indexes for table `site_settings`
+--
+ALTER TABLE `site_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `admin_details`
+--
+ALTER TABLE `admin_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `admin_role`
+--
+ALTER TABLE `admin_role`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=247;
+
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `site_settings`
+--
+ALTER TABLE `site_settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
